9cbf96d8bf1bacf47af525d543651c990cf30864
Billy <billymcfly@gmail.com>
2020-02-13 13:48:27 -0700

    Begin building solution

--- src/main/java/YahtzeeUpperSectionScoring.java
+++ src/main/java/YahtzeeUpperSectionScoring.java
@@ -1,5 +1,42 @@
 public class YahtzeeUpperSectionScoring {
+
+    private static int roll() {
+        return (int)(Math.random()*6) + 1;
+    }
+
+    private static int[] performRolls() {
+        System.out.println("Dice rolls:");
+        System.out.print("[");
+        int[] dice = new int[5];
+        for(int i = 0; i < dice.length; i++) {
+            dice[i] = roll();
+            System.out.print(dice[i]);
+            if(i < dice.length - 1) System.out.print(",");
+        }
+        System.out.println("]");
+        return dice;
+    }
+
+    private static void calculateMaxPossibleScore(int[] dice) {
+        // Get count of each number rolled
+        int[] rollCount = { 0, 0, 0, 0, 0, 0 }; // One for each possible number
+        for(int i = 0; i < dice.length; i++) {
+            rollCount[dice[i] - 1] += 1; // How many times did we roll x?
+        }
+
+        // Calculate possible scores
+        int[] possibleScores = { 0, 0, 0, 0, 0, 0 };
+        for(int i = 0; i < rollCount.length; i++) {
+            possibleScores[i] = rollCount[i] * (i + 1); // e.g. if we rolled three 2s, would calculate 3 * 2
+            if(rollCount[i] != 0) {
+                System.out.println("Number of " + (i + 1) + "s rolled: " + rollCount[i]
+                        + " - Score: " + possibleScores[i]);
+            }
+        }
+    }
+
     public static void main(String[] a) {
-        System.out.print("Hello World");
+        int[] dice = performRolls();
+        calculateMaxPossibleScore(dice);
     }
 }
